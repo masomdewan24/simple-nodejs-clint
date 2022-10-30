5613ef3bd872e8d3149afbad6477bd5f2a4f2f46
masomdewan24 <masom24@gmail.com>
2022-10-30 13:27:21 +0600

    mongodb i

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
-
 app.get('/', (req, res) =>{
     res.send('Simple Node Server is Running')
 });
@@ -15,8 +15,31 @@ const users = [
     {id: 2, name: 'Sumon', email: 'sumon@gmail.com'},
     {id: 3, name: 'Twhid', email: 'twhid@gmail.com'}
 ];
+
+// username: dbuser1
+//password: gerzOOaNv2FSyqSy
+
+
+
+const uri = "mongodb+srv://dbuser1:gerzOOaNv2FSyqSy@cluster0.6scmlus.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("simpleNode").collection("users");
+  // perform actions on the collection object
+  console.log('database connected')
+  client.close();
+});
+
 app.get('/users', (req, res) =>{
-    res.send(users);
+    if(req.query.name){
+        const search = req.query.name;
+       const filtered = users.filter(usr => usr.name.toLowerCase().indexOf(search) >= 0)
+        res.send(filtered)
+    }
+    else{
+        res.send(users);
+    }
+    
 })
 
 app.post('/users', (req, res) =>{
